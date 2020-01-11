14eb4375f27cd221e12602659d516cc792427ebf
Robb Traister <robb.traister@sciencelogic.com>
2020-01-11 17:18:51 -0500

    use hooks for amp styles

--- src/outputs/amp.tsx
+++ src/outputs/amp.tsx
@@ -2,18 +2,57 @@
 
 import React from 'react'
 
-import { App, Styles, useResource } from '@composition/composer'
+import { App, /* Resource, Styles, */ useResource, useStyles } from '@composition/composer'
+
+// const AmpOutput = () => {
+//   const resource = useResource({ name: 'public/resources/main.css' })
+
+//   return <html>
+//     <head>
+//       <meta charSet="UTF-8" />
+//       <meta name="viewport" content="width=device-width" />
+//       <title>Composition Test</title>
+//       <style amp-custom="true" dangerouslySetInnerHTML={{ __html: resource }} />
+//       <Styles inline amp-custom="true" />
+//     </head>
+//     <body>
+//       <App static />
+//     </body>
+//   </html>
+// }
+
+// const AmpOutput = () => {
+//   return <html>
+//     <head>
+//       <meta charSet="UTF-8" />
+//       <meta name="viewport" content="width=device-width" />
+//       <title>Composition Test</title>
+//       <Resource name= 'public/resources/main.css'>
+//         {({ resource }) =>
+//           <Styles amp>
+//             {({ styles, ...props }) =>
+//               <style {...props} dangerouslySetInnerHTML={{ __html: `${resource || ''}${styles || ''}` }} />
+//             }
+//           </Styles>
+//         }
+//       </Resource>
+//     </head>
+//     <body>
+//       <App static />
+//     </body>
+//   </html>
+// }
 
 const AmpOutput = () => {
-  const resource = useResource({ name: 'public/resources/main.css' })
+  const mainCss = useResource({ name: 'public/resources/main.css' })
+  const styles = useStyles()
 
   return <html>
     <head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width" />
       <title>Composition Test</title>
-      <style amp-custom="true" dangerouslySetInnerHTML={{ __html: resource }} />
-      <Styles inline amp-custom="true" />
+      <style amp-custom="true" dangerouslySetInnerHTML={{ __html: `${mainCss || ''}${styles || ''}` }} />
     </head>
     <body>
       <App static />
